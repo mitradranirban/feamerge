cdb6146fc0ab5f7a5ab8790d1abe8250a4d19bd5
Dr Anirban Mitra <mitradranirban@gmail.com>
2025-09-30 18:57:13 +0530

    Create __init__.py for fontra-feamerge

---
+++ src/fontra_feamerge/__init__.py
@@ -0,0 +1,8 @@
+"""
+fontra-feamerge - A Fontra plugin for merging multiple Adobe feature files
+"""
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"
